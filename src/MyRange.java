eb110e1cf178914d904df0e90ac6e03ca4faf985
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-06 22:00:27 +0700

    check first sign

--- src/MyRange.java
+++ src/MyRange.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class MyRange {
+    private final String input;
+
+    public MyRange(String input) {
+        this.input = input;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -9,9 +15,9 @@ public class MyRange {
         int start = Integer.parseInt(tests[0].substring(1));
         int end = Integer.parseInt(tests[1].substring(0,1));
 
-        if (start > end){
+        if (start > end) {
             System.out.println("invalid input start number has more than end number");
-            return ;
+            return;
         }
 
         if (input.startsWith("(")) {
@@ -23,8 +29,21 @@ public class MyRange {
 
         String output = "";
         for (int i = start; i <= end; i++) {
+            if ( i == start){
+                output += i;
+                continue;
+            }
             output += ", " + i;
         }
-        System.out.println(output.substring(2));
+        if (output.length() > 0){
+            System.out.println(output);
+        }else{
+            System.out.println("No value in range");
+        }
+    }
+
+
+    public boolean checkStartWithInclude() {
+        return input.startsWith("[");
     }
 }
