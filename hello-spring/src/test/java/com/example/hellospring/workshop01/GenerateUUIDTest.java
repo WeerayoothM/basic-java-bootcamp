afec871dd8c8642a2a7a34e7d8889044d079ae46
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-09 15:42:22 +0700

    Working with Overiding method

---
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/GenerateUUIDTest.java
@@ -0,0 +1,24 @@
+package com.example.hellospring.workshop01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Random7 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+}
+
+class GenerateUUIDTest {
+    @Test
+    public void getUUID(){
+        GenerateUUID generateUUID = new GenerateUUID();
+        generateUUID.setRandom(new Random7());
+        String uuid = generateUUID.get("weerayooth");
+        assertEquals("ABCweerayooth7",uuid);
+    }
+}
