9adfb5080a0925db21ba0fa8b14bfd3b2c0226cd
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-09 09:26:39 +0700

    add display name to test case

--- src/MySortTest.java
+++ src/MySortTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MySortTest {
     @Test
+    @DisplayName("Empty list should be sorted")
     public void case01() {
         List<Integer> inputArr = new ArrayList<>();
         MySort mySort = new MySort(inputArr);
@@ -17,6 +19,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[1] => [1]")
     public void case02() {
         List<Integer> inputArr = Arrays.asList(1);
         MySort mySort = new MySort(inputArr);
@@ -26,6 +29,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[1,2] => [1,2]")
     public void case03(){
         List<Integer> inputArr = Arrays.asList(1,2);
         MySort mySort = new MySort(inputArr);
@@ -35,6 +39,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[2,1] => [1,2]")
     public void case04(){
         List<Integer> inputArr = Arrays.asList(2,1);
         MySort mySort = new MySort(inputArr);
@@ -44,6 +49,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[1,2,3] => [1,2,3]")
     public void case05(){
         List<Integer> inputArr = Arrays.asList(1,2,3);
         MySort mySort = new MySort(inputArr);
@@ -53,6 +59,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[2,1,3] => [1,2,3]")
     public void case06(){
         List<Integer> inputArr = Arrays.asList(2,1,3);
         MySort mySort = new MySort(inputArr);
@@ -62,6 +69,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[3,2,1] => [1,2,3]")
     public void case07(){
         List<Integer> inputArr = Arrays.asList(3,2,1);
         MySort mySort = new MySort(inputArr);
@@ -71,6 +79,7 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("[5,4,3,2,1] => [1,2,3,4,5]")
     public void case08(){
         List<Integer> inputArr = Arrays.asList(5,4,3,2,1);
         MySort mySort = new MySort(inputArr);
