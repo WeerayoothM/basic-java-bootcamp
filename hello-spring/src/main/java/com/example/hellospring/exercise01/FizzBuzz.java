5028c30297cf6168079bd00b7382840107e88a44
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-10 01:28:40 +0700

    Refactor condition Fizzbuzz

--- hello-spring/src/main/java/com/example/hellospring/exercise01/FizzBuzz.java
+++ hello-spring/src/main/java/com/example/hellospring/exercise01/FizzBuzz.java
@@ -1,33 +1,19 @@
 package com.example.hellospring.exercise01;
 
 public class FizzBuzz {
+    // Prepare all conditions
+    private MyCondition[] conditions = new MyCondition[]{
+            new FizzBuzzCondition(),
+            new FizzCondition(),
+            new BuzzCondition(),
+            new KBTGCondition()
+    };
     public String getResult(int number) {
-        // Prepare all conditions
-        MyCondition[] conditions = new MyCondition[]{
-                new FizzBuzzCondition(),
-                new FizzCondition(),
-                new BuzzCondition(),
-                new KBTGCondition()
-        };
-
         for (MyCondition condition : conditions) {
             if (condition.check(number)){
                 return condition.print();
             }
         }
-
-//        MyCondition fizzBuzzCondition = new FizzBuzzCondition();
-//        if (fizzBuzzCondition.check(number)){
-//            return fizzBuzzCondition.print();
-//        }
-//        MyCondition fizzCondition = new FizzCondition();
-//        if (fizzCondition.check(number)){
-//            return fizzCondition.print();
-//        }
-//        MyCondition buzzCondition = new BuzzCondition();
-//        if(buzzCondition.check(number)){
-//            return buzzCondition.print();
-//        }
         return String.valueOf(number) ;
     }
 }
