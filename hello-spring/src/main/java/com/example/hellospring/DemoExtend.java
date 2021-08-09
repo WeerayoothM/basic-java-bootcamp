5d715c68b613c7ca3da075cfc7b08302ad2febfe
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-09 14:35:56 +0700

    Working with Abstract Class

---
+++ hello-spring/src/main/java/com/example/hellospring/DemoExtend.java
@@ -0,0 +1,46 @@
+package com.example.hellospring;
+
+public class DemoExtend {
+    public static void main(String[] args) {
+        Report1 report1 = new Report1();
+        report1.build();
+    }
+}
+
+class Person {
+
+}
+
+// flow or template
+abstract class Report {
+    abstract void generateHeader();
+    abstract void generateBody();
+    abstract void generateFooter();
+    public void build(){
+        generateHeader();
+        generateBody();
+        generateFooter();
+    }
+}
+
+// class which extends abstract class must have abstract method
+class Report1 extends Report{
+    @Override
+    void generateHeader() {
+        System.out.println("Generate Header");
+    }
+
+    @Override
+    void generateBody() {
+        System.out.println("Generate Body");
+    }
+
+    @Override
+    void generateFooter() {
+        System.out.println("Generate Footer");
+    }
+}
+
+interface Lego {
+
+}
