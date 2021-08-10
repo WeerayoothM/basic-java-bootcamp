f8279b2184e6a23eb5379f1425fafb5a3cb862b0
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-10 15:51:53 +0700

    Workshop Refactor : Extrack Condition to method

--- hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
@@ -1,6 +1,6 @@
 package com.example.hellospring.refactor;
 
-public class TennisGame3{
+public class TennisGame3 {
 
     private int p2;
     private int p1;
@@ -14,16 +14,41 @@ public class TennisGame3{
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+
+        // All
+        if (isAll()) {
+            return p[p1] + "-All";
         }
+
+        // Advantage
+        if (Math.abs(p1-p2) == 1 && p1 + p2 > 5){
+            s = p1 > p2 ? p1N : p2N;
+            return "Advantage " + s ;
+        }
+
+        // Deuce
+        if (isDeuce()) {
+            return "Deuce";
+        }
+
+        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+            // Normal
+            return p[p1] + "-" + p[p2];
+        }
+
+        // TODO Next
+        s = p1 > p2 ? p1N : p2N;
+        return "Win for " + s;
+
+    }
+
+    private boolean isAll() {
+        return p1 == p2 && p1 <= 2;
+    }
+
+    private boolean isDeuce() {
+        return p1 == p2 && p1 > 2;
     }
 
     public void wonPoint(String playerName) {
