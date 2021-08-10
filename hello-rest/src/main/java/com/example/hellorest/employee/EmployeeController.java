3e6310884cdf9101cbfacb8b26ccc7744de13e9f
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-10 20:39:59 +0700

    setup GetMapping/ PathVariable/ check id input

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,11 +1,27 @@
 package com.example.hellorest.employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    public EmployeeResponse getEmployeeByID(int id){
-        return new EmployeeResponse(1 , "Weerayooth","Ohm");
+    @GetMapping("/employee/{id}")
+    public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+        // Validate id => Number only
+        boolean isNumber = true;
+        for (String s : id.split("")) {
+            if (isDigit(s)) {
+                isNumber = false;
+                break;
+            }
+        }
+        int _id = isNumber ? Integer.parseInt(id) : 0;
+        return new EmployeeResponse(_id, "Weerayooth", "Ohm");
+    }
+
+    private boolean isDigit(String s) {
+        return "0123456789".indexOf(s) == -1;
     }
 }
