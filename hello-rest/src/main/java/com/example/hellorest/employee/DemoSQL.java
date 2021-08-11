61e462a7a88940cd779d39fd4d63d2e7ef6ff143
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-11 19:44:52 +0700

    Create Jpa Repository

---
+++ hello-rest/src/main/java/com/example/hellorest/employee/DemoSQL.java
@@ -0,0 +1,20 @@
+package com.example.hellorest.employee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DemoSQL {
+//    traditional
+//    public void workingWithDB(){
+//        Class.forName("driver class of database");
+//        Connection conn DriverManager.getConnection("");
+//        PreparedStatement prepStmt = conn.prepareStatement("SQL");
+//        ResultSet resultSet = prepStmt.executeQuery();
+//        while (resultSet.next()){
+//            int id = resultSet.getInt("id");
+//        }
+//    }
+
+}
