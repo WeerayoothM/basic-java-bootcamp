6691e05c769b5dfee766339c188a6043d6ffb929
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-11 10:05:02 +0700

    First case with spring boot testing

--- hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.hellorest.employee;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
@@ -9,9 +11,25 @@ import static org.springframework.boot.test.context.SpringBootTest.*;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerTest {
 
-    @Test
-    public void callApiWithPathVariable(){
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    public void callApiWithPathVariable() {
+        // convert Json to EmployeeResponse สร้างโดยใช้ default contructor
+        EmployeeResponse response = restTemplate.getForObject("/employee/1234", EmployeeResponse.class);
+        assertEquals(1234 ,response.getId());
+        assertEquals("Weerayooth",response.getFname());
+        assertEquals("Ohm",response.getLname());
+    }
+
+    @Test
+    public void callApiWithRequestParam() {
+        // convert Json to EmployeeResponse สร้างโดยใช้ default contructor
+        EmployeeResponse response = restTemplate.getForObject("/employee?id=777", EmployeeResponse.class);
+        assertEquals(777 ,response.getId());
+        assertEquals("Weerayooth",response.getFname());
+        assertEquals("Ohm",response.getLname());
     }
 
 }
