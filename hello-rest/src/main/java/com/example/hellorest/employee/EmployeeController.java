8d57d50e260f38dc1d12ec9ec42df4a5b9f40bf0
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-11 09:23:05 +0700

    Working with request parameter

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,25 +2,37 @@ package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/employee/{id}")
-    public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+//    @GetMapping("/employee/{id}")
+//    public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+//        // Validate id => Number only
+//        int _id = 0;
+//        try{
+//            _id = Integer.parseInt(id);
+//        }catch (Exception e){
+//          //Error => TODO
+//        }
+//
+//        return new EmployeeResponse(_id, "Weerayooth", "Ohm");
+//    }
+
+    // employee?id=?
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByID(@RequestParam String id) {
         // Validate id => Number only
         int _id = 0;
         try{
             _id = Integer.parseInt(id);
         }catch (Exception e){
-          //Error => TODO
+            //Error => TODO
         }
 
         return new EmployeeResponse(_id, "Weerayooth", "Ohm");
     }
 
-    private boolean isDigit(String s) {
-        return "0123456789".indexOf(s) == -1;
-    }
 }
