1d87c33a47dcce69a74b125d57c75a78d97d15a0
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-11 14:04:42 +0700

    Custom json property with jackson

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -51,6 +51,9 @@ public class EmployeeController {
 
     @PostMapping("/employee")
     public EmployeeResponse createNewEmployee(@RequestBody EmployeeRequest request){
+        // Validation  send body with wrong key
+
+
         return new EmployeeResponse(999,request.getFname(), request.getLname());
     }
 
