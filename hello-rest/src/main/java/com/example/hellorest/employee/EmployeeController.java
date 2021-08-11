095cc1cc4a8afaecccb30cb92de4792c7354b978
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-11 11:50:30 +0700

    Working with Spring autowired

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,24 +1,34 @@
 package com.example.hellorest.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class EmployeeController {
+    @Autowired
+    private MyRandom random;
 
-    @GetMapping("/employee/{id2}")
-    public EmployeeResponse getEmployeeByID(@PathVariable(name="id2") String id) {
+    @GetMapping("/employee/{id}")
+    public EmployeeResponse getEmployeeByID(@PathVariable String id) {
         // Validate id => Number only
         int _id = 0;
         try{
+            System.out.println(Integer.parseInt(id));
             _id = Integer.parseInt(id);
         }catch (Exception e){
           //Error => TODO
         }
 
-        return new EmployeeResponse(_id, "Weerayooth", "Ohm");
+        // Workshop
+
+        int number = random.nextInt(10);
+
+        return new EmployeeResponse(_id, "Weerayooth" + number, "Ohm");
     }
 
     // employee?id2=?
--- hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -5,8 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
+
+
+// Overide by method
+class Random7 extends MyRandom {
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+}
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerTest {
@@ -19,10 +30,10 @@ public class EmployeeControllerTest {
         // convert Json to EmployeeResponse สร้างโดยใช้ default contructor
         EmployeeResponse response = restTemplate.getForObject("/employee/123", EmployeeResponse.class);
 
-        EmployeeResponse expected = new EmployeeResponse(123,"Weerayooth","Ohm");
+        EmployeeResponse expected = new EmployeeResponse(123,"Weerayooth7","Ohm");
 
         assertEquals(123 ,response.getId());
-        assertEquals("Weerayooth",response.getFname());
+        assertEquals("Weerayooth7",response.getFname());
         assertEquals("Ohm",response.getLname());
         assertEquals(expected,response);
     }
