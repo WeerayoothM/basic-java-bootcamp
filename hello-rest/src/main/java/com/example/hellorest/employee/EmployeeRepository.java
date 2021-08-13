9ee72067d09f05d660c01fa8809462fc9766c536
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-13 14:32:53 +0700

    Testing with JPA

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeRepository.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.example.hellorest.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// <Employee, Integer> := < map Entity? , primary key? >
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 }
---
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public void foundWithID1(){
+        Employee employee1 = repository.getById(1);
+        assertEquals(1,employee1.getId());
+        assertEquals("Weerayooth from DB",employee1.getFirstName());
+        assertEquals("Ohm from DB",employee1.getLastName());
+    }
+}
